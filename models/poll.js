4aa75262f785b4630113551fcf340a53d91f76d4
Chidoziee <130702901+Chidoziee@users.noreply.github.com>
2023-04-13 21:10:09 +0100

    Add files via upload

---
+++ models/poll.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const optionSChema = new mongoose.Schema({
+  option: String,
+  votes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const pollSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  question: String,
+  options: [optionSChema],
+  voted: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  created: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+const Poll = mongoose.model('Poll', pollSchema);
+
+module.exports = Poll
